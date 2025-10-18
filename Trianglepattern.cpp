3dd2eb874a04221f25c67b5151c317a5bb19bb5c
Asma Chowdhury <asmachowdhury4563@gmail.com>
2025-10-18 02:40:38 -1200

    triangle pattern printing in Trianglepattern.cpp
    Implement a program to print a triangle pattern.

---
+++ Trianglepattern.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int rows = 5; 
+    
+    // Outer loop for rows
+    for (int i = 1; i <= rows; ++i) {
+        
+        // Inner loop for columns 
+        for (int j = 1; j <= i; ++j) {
+            cout << "* ";
+        }
+        
+        cout << endl; 
+    }
+    
+    return 0;
+}
