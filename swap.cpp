c0591001f1329949f6f17754e7963b76a4fc0465
Asma Chowdhury <asmachowdhury4563@gmail.com>
2025-10-18 03:12:51 -1200

    Implement number swapping using pointers
    This program prompts the user to enter two numbers and swaps their values using pointers.

---
+++ swap.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+// Function to swap two numbers using pointers
+void swapNumbers(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int x, y;
+    cout << "Enter two numbers: ";
+    cin >> x >> y;
+
+    cout << "Before swapping: x = " << x << ", y = " << y << endl;
+
+    swapNumbers(&x, &y);
+
+    cout << "After swapping: x = " << x << ", y = " << y << endl;
+    return 0;
+}
